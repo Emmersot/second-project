a14797858ce84efa8247c347211ef08018f30a70
Tyler Emmerson <TylerEmmerson0216@gmail.com>
2022-06-11 16:49:01 -0700

    fix the code to show and hide, create values that change to different answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,26 @@
-
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   $(document).ready(function() {
-      const age = parseInt(prompt("How old are you? 1-3"));
-    
-      if (age > 3) {
-        alert("LOL")
-        $('#LOL').show();
-      } else if (age === 1) {
-        $('#Swift').show();
-      } else if (age === 2) {
-        $('#Python').show();
-      } else if (age === 3) {
-        $('#Ruby').show();
-      
-}
-    });
-    $("form#subtract").submit(function(event) {
-      event.preventDefault();
-      const number1 = parseInt($("#subtract1").val());
-      const number2 = parseInt($("#subtract2").val());
-      const result = subtract(number1, number2);
-      $("#output").text(result);
-    });
-    
-    $("form#multiply").submit(function(event) {
-      event.preventDefault();
-      const number1 = parseInt($("#multiply1").val());
-      const number2 = parseInt($("#multiply2").val());
-      const result = multiply(number1, number2);
-      $("#output").text(result);
-    });
-  
-
+$(document).ready(function() {
+$("form#user").submit(function(event) {
+  event.preventDefault()
+  const colorVal = parseInt($("#color").val());
+    if (colorVal === 1) 
+    {
+    $('#Swift').show();
+    $('#Python').hide();
+    $('#Ruby').hide();
+    } 
+    else if (colorVal === 2) 
+    {
+    $('#Python').show();
+    $('#Swift').hide();
+    $('#Ruby').hide();
+    } 
+    else if (colorVal === 3) 
+    {
+    $('#Ruby').show();
+    $('#Swift').hide();
+    $('#Python').hide();
+    }
+    $(".questions").hide();
+    $(".button").hide();
+  });
+});
